a0869cea1d4dde97eb1b26a7ffb8a7ac0b7a05ae
Rakioth <rakioths@gmail.com>
2024-04-21 12:22:42 +0200

    🧱 Added Components

---
+++ src/consts/mobile-menu.ts
@@ -0,0 +1,2 @@
+export const MOBILE_MENU_BUTTON_ID = "menu-toggle"
+export const MOBILE_MENU_CONTENT_ID = "mobile-menu-content"
---
+++ src/consts/theme.ts
@@ -0,0 +1,8 @@
+export const THEME_COLOR = {
+	DARK: "#ec7df5",
+	LIGHT: "#a621aa",
+}
+export const BACKGROUND_COLOR = {
+	DARK: "#0f004a",
+	LIGHT: "#f3f2f9",
+}
