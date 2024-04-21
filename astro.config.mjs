4eaa38c781442dfa33235853df639ef436852731
Rakioth <rakioths@gmail.com>
2024-04-21 12:06:38 +0200

    🛠️ Added Config Files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,26 @@
-import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+	site: "https://raks.dev",
+	integrations: [tailwind()],
+	compressHTML: true,
+	vite: {
+		build: {
+			cssMinify: "lightningcss",
+		},
+	},
+	build: {
+		inlineStylesheets: "always",
+	},
+	devToolbar: {
+		enabled: false,
+	},
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "es"],
+		routing: {
+			prefixDefaultLocale: true,
+		},
+	},
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,31 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @type {import('tailwindcss').Config}
+ */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					50: "#fef4ff",
+					100: "#fbe9fe",
+					200: "#f7d1fd",
+					300: "#f2aef9",
+					400: "#ec7df5",
+					500: "#de4bea",
+					600: "#ce3ed6",
+					700: "#a621aa",
+					800: "#891d8b",
+					900: "#721d72",
+					950: "#4c064c",
+				},
+			},
+		},
 	},
-	plugins: [],
+	darkMode: "class",
+	plugins: [
+		function ({ addVariant }) {
+			addVariant("any-hover", "@media (any-hover: hover) { &:hover }")
+		},
+	],
 }
